40c3f5fbeff79a1014668ce8861b2b3a22725642
GenesisVergaraA <genesis.vergara.7e6@itb.cat>
2024-12-12 16:19:54 +0100

    tests ejercicio 5 modificados

--- src/test/kotlin/models/ejercicio5.kt
+++ src/test/kotlin/models/ejercicio5.kt
@@ -3,22 +3,22 @@ import Pila
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.*
 
-class ej5 {
+class ejercicio5 {
     @Test
-    fun testPush() {
+    fun testPush(){
         val pila = Pila()
         pila.push(5)
         pila.push(10)
         assertEquals(5, pila.piles[0])
         assertEquals(10, pila.piles[1])
-        assertEquals(1, pila.primer)
     }
 
     @Test
     fun testPush2() {
         val pila = Pila()
-        repeat(10) { pila.push(it) }
-        pila.push(11)
+        repeat(10){
+            pila.push(it)
+        }
         assertEquals(9, pila.piles[9])
         assertEquals(9, pila.primer)
     }
@@ -26,16 +26,16 @@ class ej5 {
     @Test
     fun testPop() {
         val pila = Pila()
-        pila.push(1)
-        pila.push(10)
-        assertEquals(10, pila.piles[1])
-        assertEquals(1, pila.primer)
-    }
-
-    @Test
-    fun testPop2() {
-        val pila = Pila()
         pila.pop()
         assertEquals(-1, pila.primer)
     }
+    @Test
+    fun testPop2() {
+        val pila = Pila()
+        repeat(5){
+            pila.push(it)
+        }
+        assertEquals(4,pila.primer)
+    }
+
 }
